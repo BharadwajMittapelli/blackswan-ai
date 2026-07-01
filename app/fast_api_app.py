5dc79d3c4b6ea1011bbbe1e129f5bd7f46ca50ef
Bharadwaj Mittapelli <bharadwajmittapelli@gmail.com>
2026-07-01 13:56:09 +0530

    Deploy backend CORS fix and frontend updates

--- app/fast_api_app.py
+++ app/fast_api_app.py
@@ -61,7 +61,7 @@ CACHE_TTL = 21600
 fast_app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
